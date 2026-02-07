faf336aa9e4d8a683f842c29721d3973af13c3cf
thevaishndra <chandrav306m@gmail.com>
2026-02-07 12:45:06 +0530

    fastify server and plugin eco system

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
 require("dotenv").config();
+const path = require("path")
 const fastify = require("fastify")({logger : true});
+const fastifyEnv = require("@fastify/env");
+import cors from "@fastify/cors";
+
+//define cors
+// await fastify.register(cors, {
+//   // put your options here
+// });
+
+//register plugin
+fastify.register(require("@fastify/cors"))
+fastify.register(require("@fastify/sensible"));
+fastify.register(require("@fastify/env"), {
+    dotenv : true,
+    schema : {
+        type : "object",
+        required : ["PORT", "MONGODB_URI", "JWT_TOKEN"],
+        properties : {
+            PORT : {type : "string", default : 3000},
+            MONGODB_URI : { type : "string"},
+            JWT_TOKEN : { type : "string"},
+        }
+    }
+});
 
 //declare a route
 fastify.get("/", function(request, reply){
     reply.send({hello : "world"});
+});
+
+const schema = {
+  type: "object",
+  required: ["PORT"],
+  properties: {
+    PORT: {
+      type: "string",
+      default: 3000,
+    },
+  },
+};
+
+const options = {
+  confKey: "config", // optional, default: 'config'
+  schema: schema,
+  data: data, // optional, default: process.env
+};
+
+fastify.register(fastifyEnv, options).ready((err) => {
+  if (err) console.error(err);
+
+  console.log(fastify.config); // or fastify[options.confKey]
+  console.log(fastify.getEnvs());
+  // output: { PORT: 3000 }
 });
 
 const start = async () => {
